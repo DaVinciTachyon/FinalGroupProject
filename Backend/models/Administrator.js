c94d734441c3d566dafa0831023575bd33361f50
Michael McGuinness <michael.mcguinness@utmost.co>
2021-02-17 17:30:11 +0100

    admin register and login

---
+++ Backend/models/Administrator.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+module.exports = mongoose.model('Administrator', new mongoose.Schema({
+    id: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    name: {
+        type: String,
+        min: 3,
+        required: true
+    },
+	email: {
+        type: String,
+        min: 6,
+        required: true
+    },
+    password: {
+        type: String,
+        min: 6,
+        required: true
+    },
+    dateJoined: {
+        type: Date,
+        default: Date.now()
+    },
+}));
