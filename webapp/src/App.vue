4b6d1879815d00752baddcf18580f46942b7911e
Davy Nolan <dnolan5@tcd.ie>
2021-02-22 15:13:45 +0000

    web interface set up

---
+++ webapp/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+    <h1>Admin Hub</h1>
+    </v-app-bar>
+
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+
+  </v-app>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ webapp/src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import ManageUsers from '../views/ManageUsers.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/users',
+    name: 'ManageUsers',
+    component: ManageUsers
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
+
+export default router
