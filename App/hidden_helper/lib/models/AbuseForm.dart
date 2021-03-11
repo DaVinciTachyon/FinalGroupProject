739988c77004a668f4a8dc8231d076abaa2def94
darts <darts@tcd.ie>
2021-03-11 18:18:52 +0000

    Form object for UI

--- App/hidden_helper/lib/models/AbuseForm.dart
+++ App/hidden_helper/lib/models/AbuseForm.dart
@@ -1,9 +1,11 @@
 //This should probably be 'sex'
+import 'package:flutter/foundation.dart';
+
 enum GenderEnum { male, female }
 
-extension ParseToGenderString on GenderEnum {
+extension GenderOps on GenderEnum {
   String toGenderString() {
-    return this.toString().split('.').last.capitaliseFirst();
+    return toSimpleCapitalEnum(this);
   }
 }
 
@@ -38,9 +40,36 @@ extension ParseToAgeString on AgeRangeEnum {
         return "55-64";
       case AgeRangeEnum.sixtyfiveToSeventyfour:
         return "65-74";
-      default:
+      case AgeRangeEnum.overSeventyfive:
         return "75+";
+      default:
+        throw Exception("Could not parse age enum to string!");
     }
+  }
+}
+
+AgeRangeEnum parseAgeFromString(enumStr) {
+  switch (enumStr) {
+    case "<12":
+      return AgeRangeEnum.underTwelve;
+    case "12-17":
+      return AgeRangeEnum.twelveToSeventeen;
+    case "18-24":
+      return AgeRangeEnum.eighteenToTwentyfour;
+    case "25-34":
+      return AgeRangeEnum.twentyfiveToThirtyfour;
+    case "35-44":
+      return AgeRangeEnum.thirtyfiveToFourtyfour;
+    case "45-54":
+      return AgeRangeEnum.fourtyfiveToFiftyfour;
+    case "55-64":
+      return AgeRangeEnum.fiftyfiveToSixtyfour;
+    case "65-74":
+      return AgeRangeEnum.sixtyfiveToSeventyfour;
+    case "75+":
+      return AgeRangeEnum.overSeventyfive;
+    default:
+      throw Exception("Could not parse string to age enum!");
   }
 }
 
@@ -55,7 +84,7 @@ enum SeekedAttentionEnum {
 
 extension ParseToSeekedAttentionString on SeekedAttentionEnum {
   String toSeekedAttentionString() {
-    return this.toString().split('.').last.capitaliseFirst();
+    return toSimpleCapitalEnum(this);
   }
 }
 
@@ -70,7 +99,7 @@ enum OfferedAttentionEnum {
 
 extension ParseToOfferedAttentionString on OfferedAttentionEnum {
   String toOfferedAttentionString() {
-    return this.toString().split('.').last.capitaliseFirst();
+    return toSimpleCapitalEnum(this);
   }
 }
 
@@ -78,7 +107,7 @@ enum ReferredAttentionEnum { legal, psychosocial, medical, other }
 
 extension ParseToReferredAttentionString on ReferredAttentionEnum {
   String toReferredAttentionString() {
-    return this.toString().split('.').last.capitaliseFirst();
+    return toSimpleCapitalEnum(this);
   }
 }
 
@@ -96,7 +125,7 @@ enum PhysicalAbuseClassificationEnum {
 extension ParseToPhysicalAbuseClassificationString
     on PhysicalAbuseClassificationEnum {
   String toPhysicalAbuseClassificationString() {
-    return this.toString().split('.').last.capitaliseFirst();
+    return toSimpleCapitalEnum(this);
   }
 }
 
@@ -118,7 +147,7 @@ enum PsychologicalAbuseClassificationEnum {
 extension ParseToPsychologicalAbuseClassificationString
     on PsychologicalAbuseClassificationEnum {
   String toPsychologicalAbuseClassificationString() {
-    return this.toString().split('.').last.capitaliseFirst();
+    return toSimpleCapitalEnum(this);
   }
 }
 
@@ -128,10 +157,11 @@ enum SexualAbuseClassificationEnum {
   attemptedRape,
   rape
 }
+
 extension ParseToSexualAbuseClassificationString
     on SexualAbuseClassificationEnum {
   String toSexualAbuseClassificationString() {
-    return this.toString().split('.').last.capitaliseFirst();
+    return toSimpleCapitalEnum(this);
   }
 }
 
@@ -143,9 +173,10 @@ enum RightsEnum {
   resourceConflict,
   other
 }
+
 extension ParseToRightsString on RightsEnum {
   String toRightsString() {
-    return this.toString().split('.').last.capitaliseFirst();
+    return toSimpleCapitalEnum(this);
   }
 }
 
@@ -158,116 +189,50 @@ enum RelationshipEnum {
   association,
   other
 }
+
 extension ParseToRelationshipString on RelationshipEnum {
   String toRelationshipString() {
-    return this.toString().split('.').last.capitaliseFirst();
+    return toSimpleCapitalEnum(this);
   }
 }
 
-
-//Dart only supports library-level privacy, don't use this
-// class Attention {
-//   bool legal;
-//   bool psychosocial;
-//   bool medical;
-//   bool other;
-//   String otherDetails;
-
-//   Attention(this.legal, this.psychosocial, this.medical, this.other,
-//       this.otherDetails);
-// }
-
-// class ReferredAttention extends Attention {
-//   ReferredAttention(legal, psychosocial, medical, other, otherDetails)
-//       : super(legal, psychosocial, medical, other, otherDetails);
-// }
-
-// class AttentionSeeked extends Attention {
-//   bool refuge;
-//   bool information;
-
-//   AttentionSeeked(this.information, legal, psychosocial, medical, this.refuge,
-//       other, otherDetails)
-//       : super(legal, psychosocial, medical, other, otherDetails);
-// }
-
-// class AttentionOffered extends Attention {
-//   bool refuge;
-//   bool information;
-
-//   AttentionOffered(this.information, legal, psychosocial, medical, this.refuge,
-//       other, otherDetails)
-//       : super(legal, psychosocial, medical, other, otherDetails);
-// }
-
-class Rights {
-  bool economicResources;
-  bool opportunities;
-  bool education;
-  bool healthcare;
-  bool resourceConflict;
-  bool other;
-  String otherDetails;
-
-  Rights(this.economicResources, this.opportunities, this.education,
-      this.healthcare, this.resourceConflict, this.other, this.otherDetails);
-}
-
-class CaseClassification {
-  bool physicalHitting,
-      physicalPushing,
-      physicalChoking,
-      physicalCutting,
-      physicalBurning,
-      physicalWeaponUsage,
-      physicalAcid,
-      physicalOtherPainfulAttack;
-  bool psychologicalViolenceThreats,
-      psychologicalMurderThreats,
-      psychologicalIntimidation,
-      psychologicalHumiliation,
-      psychologicalIsolation,
-      psychologicalStalking,
-      psychologicalVerbalHarrassment,
-      psychologicalUnwantedAttention,
-      psychologicalNonVerbalThreats;
-}
-
-class Perpetrator {
-  GenderEnum gender;
-  bool isKnown;
-  RelationshipEnum relationShip;
-
-  Perpetrator(this.gender, this.isKnown, this.relationShip);
-}
-
 class AbuseForm {
-  //tmp
-  String type;
-  String location;
-
   DateTime incidentDate;
   DateTime attentionDate;
   GenderEnum gender;
   AgeRangeEnum ageRange;
   String municipality;
   String community;
-  // AttentionSeeked seekedAttention;
-  // AttentionOffered offeredAttention;
-  CaseClassification classification; //TODO
-  Rights rightDenied;
-  Perpetrator perpetrator;
+  List<SeekedAttentionEnum> seekedAttention;
+  List<OfferedAttentionEnum> offeredAttention;
+  List<ReferredAttentionEnum> referredAttention;
+  List<PhysicalAbuseClassificationEnum> physicalAbuse;
+  List<PsychologicalAbuseClassificationEnum> psychologicalAbuse;
+  List<SexualAbuseClassificationEnum> sexualAbuse;
+  bool forcedMarriage;
+  List<RightsEnum> rightsDenied;
 
-  // AbuseForm(this.incidentDate, this.attentionDate, this.gender, this.ageRange,
-  //     this.municipality, this.community, this.seekedAttention,
-  //     this.offeredAttention, this.classification, this.rightDenied,
-  //     this.perpetrator);
-
-  AbuseForm(this.type, this.location);
+  AbuseForm();
 }
 
 extension StringExtension on String {
   String capitaliseFirst() {
     return "${this[0].toUpperCase()}${this.substring(1)}";
   }
+}
+
+extension ToggleExtension on List {
+  void toggleElement(elem) {
+    this.contains(elem) ? this.remove(elem) : this.add(elem);
+  }
+}
+
+dynamic enumFromString(String enumStr, enumClass) {
+  String lwrStr = enumStr.toLowerCase();
+  return enumClass.values
+      .firstWhere((e) => describeEnum(e).toLowerCase() == lwrStr);
+}
+
+String toSimpleCapitalEnum(enumVal) {
+  return describeEnum(enumVal).split('.').last.capitaliseFirst();
 }
