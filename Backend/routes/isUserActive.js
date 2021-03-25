0f2e7adb3bf4cffc10c8e03405b1ac0a4db9e014
DaVinciTachyon <davincitachyon@gmail.com>
2021-03-25 12:51:39 +0000

    activate/deactivate

--- Backend/routes/isUserActive.js
+++ Backend/routes/isUserActive.js
@@ -1,7 +1,10 @@
 const Administrator = require("../models/Administrator");
 const Monitor = require('../models/Monitor');
 
-module.exports.monitor = async (req, res, next) => {
+monitor = {}
+administrator = {}
+
+monitor.isActive = async (req, res, next) => {
 	const user = await Monitor.findOne({ email: req.body.email });
 	if(!user)
         return res.status(400).send({ error: 'Email does not exist' });
@@ -12,7 +15,18 @@ module.exports.monitor = async (req, res, next) => {
     next();
 };
 
-module.exports.administrator = async (req, res, next) => {
+monitor.isInactive = async (req, res, next) => {
+	const user = await Monitor.findOne({ email: req.body.email });
+	if(!user)
+        return res.status(400).send({ error: 'Email does not exist' });
+
+	if(user.active)
+		return res.status(400).send({ error: 'User active' });
+    
+    next();
+};
+
+administrator.isActive = async (req, res, next) => {
 	const user = await Administrator.findOne({ email: req.body.email });
 	if(!user)
         return res.status(400).send({ error: 'Email does not exist' });
@@ -22,3 +36,19 @@ module.exports.administrator = async (req, res, next) => {
     
     next();
 };
+
+administrator.isInactive = async (req, res, next) => {
+	const user = await Administrator.findOne({ email: req.body.email });
+	if(!user)
+        return res.status(400).send({ error: 'Email does not exist' });
+
+	if(user.active)
+		return res.status(400).send({ error: 'User active' });
+    
+    next();
+};
+
+module.exports = {
+	monitor,
+	administrator
+}
