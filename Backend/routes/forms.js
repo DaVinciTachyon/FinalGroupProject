bde2273dbb37ce04deaab84cb66e254a5048b09d
DaVinciTachyon <davincitachyon@gmail.com>
2021-04-13 17:58:01 +0100

    id extratcion

--- Backend/routes/forms.js
+++ Backend/routes/forms.js
@@ -7,7 +7,21 @@ router.use(function(req, res, next) {
 });
 
 router.post('/', verify.monitor, async (req, res) => {
-    const form = new Form(req.body);
+    const form = new Form({
+        monitorID: req.user.id,
+        incidentDate: req.body.incidentDate,
+        attentionDate: req.body.attentionDate,
+        gender: req.body.gender,
+        ageRange: req.body.ageRange,
+        municipality: req.body.municipality,
+        community: req.body.community,
+        seekedAttention: req.body.seekedAttention,
+        offeredAttention: req.body.offeredAttention,
+        referredAttention: req.body.referredAttention,
+        classification: req.body.classification,
+        rightDenied: req.body.rightDenied,
+        perpetrator: req.body.perpetrator
+    });
     try {
         await form.save();
         res.sendStatus(200);
