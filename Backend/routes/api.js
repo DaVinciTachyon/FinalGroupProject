dc7c687c214ccb2c6e793d35dc3595a2939ff269
DaVinciTachyon <davincitachyon@gmail.com>
2021-04-13 19:08:28 +0100

    added notes endpoints

--- Backend/routes/api.js
+++ Backend/routes/api.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const formsRoute = require('./forms');
+const notesRoute = require('./notes');
 const monitorsRoute = require('./monitors');
 const adminAuthRoute = require('./adminAuth');
 const monitorAuthRoute = require('./monitorAuth');
@@ -9,6 +10,7 @@ router.use(function(req, res, next) {
 });
 
 router.use('/forms', formsRoute);
+router.use('/notes', notesRoute);
 router.use('/monitors', monitorsRoute);
 router.use('/administrator', adminAuthRoute);
 router.use('/monitor', monitorAuthRoute);
